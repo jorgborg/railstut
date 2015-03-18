4e7b80f20d5bb2ef2e78ef9fca04f33e90c2691b
Jorge Jimenez <jorge.jimenez@gmail.com>
2015-03-18 14:54:45 -0700

    rails generate controller websiteCore index events shop discussion

---
+++ app/controllers/website_core_controller.rb
@@ -0,0 +1,13 @@
+class WebsiteCoreController < ApplicationController
+  def index
+  end
+
+  def events
+  end
+
+  def shop
+  end
+
+  def discussion
+  end
+end
---
+++ app/helpers/website_core_helper.rb
@@ -0,0 +1,2 @@
+module WebsiteCoreHelper
+end
